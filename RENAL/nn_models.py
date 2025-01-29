64e4c3f2fb545c5ea6af5ec760f2cd4847f88e0d
aoranzhangmia <108651236+aoranzhangmia@users.noreply.github.com>
2025-01-29 16:59:17 -0500

    Update nn_models.py

--- RENAL/nn_models.py
+++ RENAL/nn_models.py
@@ -384,6 +384,9 @@ def model_train(train_data, test_data, model, config, mae_eval=False, ts=None,
     return model, train_llks, test_llks, test_maes
 
 class NeuralHawkes(nn.Module):
+    """
+    Neural Hawkes Network (https://github.com/Hongrui24/NeuralHawkesPytorch; https://github.com/hongyuanmei/neurawkes) proposed by Hongyuan Mei and Jason Eisner (2017).
+    """
     def __init__(self, config):
         super(NeuralHawkes, self).__init__()
         self.T0 = config.T0
